5f576b2825666685f71ced6665f1c4d265d6cf18
Rodrigo Guedes <142233721+guedes-rodrigo@users.noreply.github.com>
2024-02-26 14:35:28 +0000

    Add database.js and .env

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,19 @@
 import { Client } from "pg";
 
-async function query(queryObject){
-    const client = new Client({
-        host: 'localhost',
-        port: 5432,
-        database: 'postgres',
-        user: 'postgres',
-        password: 'local_password',
-    }
-    );
-    await client.connect();
-    const result = await client.query(queryObject);
-    await client.end();
-    return result;
-
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
 }
 
 export default {
-    query: query,
-}
+  query: query,
+};
